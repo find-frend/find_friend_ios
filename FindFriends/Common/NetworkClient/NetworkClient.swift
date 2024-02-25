7e2386272538387cce1977231cdb44d0261ea11e
Vitaly Alekseev <valekseev@yandex.ru>
2024-02-25 19:02:08 +0300

    #13 Реализация сетевого слоя и 2 сервиса для работы с Friends и Users

---
+++ FindFriends/Common/NetworkClient/NetworkClient.swift
@@ -0,0 +1,122 @@
+import Foundation
+
+enum NetworkClientError: Error {
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
+    case urlSessionError
+    case parsingError
+}
+
+protocol NetworkClient {
+    @discardableResult
+    func send(request: NetworkRequest,
+              onResponse: @escaping (Result<Data, Error>) -> Void) -> NetworkTask?
+
+    @discardableResult
+    func send<T: Decodable>(request: NetworkRequest,
+                            type: T.Type,
+                            onResponse: @escaping (Result<T, Error>) -> Void) -> NetworkTask?
+}
+
+struct DefaultNetworkClient: NetworkClient {
+    private let session: URLSession
+    private let decoder: JSONDecoder
+    private let encoder: JSONEncoder
+
+    init(session: URLSession = URLSession.shared,
+         decoder: JSONDecoder = JSONDecoder(),
+         encoder: JSONEncoder = JSONEncoder()) {
+        self.session = session
+        self.decoder = decoder
+        self.encoder = encoder
+    }
+
+    @discardableResult
+    func send(request: NetworkRequest, onResponse: @escaping (Result<Data, Error>) -> Void) -> NetworkTask? {
+        guard let urlRequest = create(request: request) else { return nil }
+//
+//        print("urlRequest: \(urlRequest.debugDescription)")
+//        print("urlRequest: \(urlRequest.httpMethod)")
+//        let str = String(decoding: urlRequest.httpBody ?? Data(), as: UTF8.self)
+//        print("urlRequest: \(str)")
+//        print("urlRequest: \(urlRequest.allHTTPHeaderFields)")
+//        
+
+        let task = session.dataTask(with: urlRequest) { data, response, error in
+            guard let response = response as? HTTPURLResponse else {
+                onResponse(.failure(NetworkClientError.urlSessionError))
+                return
+            }
+            
+            let strData = String(decoding: data!, as: UTF8.self)
+//            print("response: \(strData)")
+//            print("response: \(response.statusCode)")
+            
+            guard 200 ..< 300 ~= response.statusCode else {
+                onResponse(.failure(NetworkClientError.httpStatusCode(response.statusCode)))
+                return
+            }
+
+            if let data = data {
+                onResponse(.success(data))
+                return
+            } else if let error = error {
+                onResponse(.failure(NetworkClientError.urlRequestError(error)))
+                return
+            } else {
+                assertionFailure("Unexpected condition!")
+                return
+            }
+        }
+
+        task.resume()
+
+        return DefaultNetworkTask(dataTask: task)
+    }
+
+    @discardableResult
+    func send<T: Decodable>(request: NetworkRequest, type: T.Type, onResponse: @escaping (Result<T, Error>) -> Void) -> NetworkTask? {
+        return send(request: request) { result in
+            switch result {
+            case let .success(data):
+                self.parse(data: data, type: type, onResponse: onResponse)
+            case let .failure(error):
+                onResponse(.failure(error))
+            }
+        }
+    }
+
+    // MARK: - Private
+
+    private func create(request: NetworkRequest) -> URLRequest? {
+        guard let endpoint = request.endpoint else {
+            assertionFailure("Empty endpoint")
+            return nil
+        }
+
+        var urlRequest = URLRequest(url: endpoint)
+        urlRequest.httpMethod = request.httpMethod.rawValue
+        
+        if request.token != "" {
+            urlRequest.setValue("Token \(request.token)", forHTTPHeaderField: "Authorization")
+        }
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        if let dto = request.dto,
+           let dtoEncoded = try? encoder.encode(dto) {
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            urlRequest.httpBody = dtoEncoded
+        }
+
+        return urlRequest
+    }
+
+    private func parse<T: Decodable>(data: Data, type _: T.Type, onResponse: @escaping (Result<T, Error>) -> Void) {
+        do {
+            let response = try decoder.decode(T.self, from: data)
+            onResponse(.success(response))
+        } catch {
+            onResponse(.failure(NetworkClientError.parsingError))
+        }
+    }
+}
