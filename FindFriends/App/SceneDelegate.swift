230c91f7749896fbda024982ba0fc61266e7f757
udachi tomo <fragnet1ck@gmail.com>
2024-03-06 19:17:56 +0300

    no message

--- FindFriends/App/SceneDelegate.swift
+++ FindFriends/App/SceneDelegate.swift
@@ -15,8 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        let navigationController = UINavigationController(rootViewController: CityViewController())
-        window?.rootViewController = navigationController
+        window?.rootViewController = SplashViewController()
         window?.makeKeyAndVisible()
     }
 }
